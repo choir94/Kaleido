d7bb926b7706d393dc6d39f1062c16f5301be531
Ahmad Choiruddin <83292508+choir94@users.noreply.github.com>
2025-02-05 20:20:47 +0700

    Create banner.js

---
+++ banner.js
@@ -0,0 +1,25 @@
+// banner.js 
+import chalk from 'chalk';
+
+export function displayBanner() {
+
+    console.log(chalk.cyan(`
+
+           _         _                   _   _           _      
+     /\   (_)       | |                 | \ | |         | |     
+    /  \   _ _ __ __| |_ __ ___  _ __   |  \| | ___   __| | ___ 
+   / /\ \ | | '__/ _\` | '__/ _ \| '_ \  | . \` |/ _ \ / _\` |/ _ \\
+  / ____ \| | | | (_| | | | (_) | |_) | | |\  | (_) | (_| |  __/
+ /_/    \_\_|_|  \__,_|_|  \___/| .__/  |_| \_|\___/ \__,_|\___|
+                                | |                             
+                                |_|                             
+
+        GitHub saya: github.com/choir94
+
+                   ${chalk.yellow('KALAEIDO')}                
+
+     📢  ${chalk.blue('Saluran Telegram: https://t.me/airdrop_node')}
+
+    `));
+
+}
